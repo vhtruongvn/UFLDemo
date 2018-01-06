7f3c2af9d389665ec6e66d5845e968b48dfe8f2d
Truong Vo <vhtruongvn@gmail.com>
2018-01-06 11:15:59 +0700

    init project

---
+++ UFLDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UFLDemo
+//
+//  Created by Truong Vo on 1/5/18.
+//  Copyright © 2018 Truong Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
